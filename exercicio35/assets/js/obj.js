085924e60b929bd787eea2f3581a9423438fa0f7
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2021-06-24 19:07:01 -0300

    estudo de object.defineProperty

--- exercicio35/assets/js/obj.js
+++ exercicio35/assets/js/obj.js
@@ -1,15 +1,30 @@
 // defineProperty - defineProperties
 
 function Produto (nome, preco, estoque) {
-    this.nome = nome;
-    this.preco = preco;
+    /* this.nome = nome;
+    this.preco = preco; */
     
     Object.defineProperty(this, 'estoque', {
-        enumerable: true,
-        value: estoque,
-        writable: false,
-        configurable: true
+        enumerable: true, //mostra a chave
+        value: estoque, //Valor
+        writable: false, // pode ou não ser alterado
+        configurable: true // pode ou não ser configuravel
     });
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: false,
+            configurable: true
+        },
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: false,
+            configurable: true
+        },
+    })
 }
 
 const produto1 = new Produto('Camiseta', 20, 3);
