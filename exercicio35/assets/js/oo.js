b10b32345ae5ad40220ad15489348a66b4308885
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2021-06-27 12:37:28 -0300

    Trabalhando com herença, prototype

--- exercicio35/assets/js/oo.js
+++ exercicio35/assets/js/oo.js
@@ -14,7 +14,7 @@ const data = new Date();
 console.dir(pessoa1);
 
 // ============================= utilzando o prototype para passar os metodos 
-function Produto(nome, preco) {
+/* function Produto(nome, preco) {
     this.nome = nome;
     this.preco = preco;
 
@@ -30,4 +30,59 @@ Produto.prototype.aumento = function(percentual) {
 
 const p1 = new Produto('Camiseta', 50);
 p1.desconto(100);
-console.log(p1);
+console.log(p1); */
+
+// ===================== TRABALHANDO COM HERANÇA
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia;
+};
+
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia;
+};
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+    this.cor = cor;
+}
+
+Camiseta.prototype = Object.create(Produto.prototype); //criei um objeto vazio, para linkar o prototype de produto ao prototype de camiseta.
+Camiseta.prototype.constructor = Camiseta; //informo que a função contrututora de camiseta é camiseta e não o Produto.
+
+// sobreescrevendo metodo aumento de produto para dentro do proto de camisete
+Camiseta.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+            return estoque;
+        },
+        set: function(){
+            if(typeof valor !== 'number') return;
+            estoque = valor;
+        },
+    })
+};
+
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('gen', 111);
+const camiseta = new Camiseta('regata', 7.5, 'preta');
+
+camiseta.desconto(5);
+
+console.log(produto);
+console.log(camiseta);
